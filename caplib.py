6aa0625b9a307b55cc736bb5886875c5beecf60b
jgpeza <jgpeza@gmail.com>
2021-10-20 21:33:59 -0500

    Add files via upload

---
+++ caplib.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""
+This module is a library with all the functions needed to query CENCOR infrastructure
+in order to gather data related to resorce usage in the network devices, so this can be used
+as a source of information for generating capacity plan reports
+"""
+
+#Imports
+
+#Module constants
+
+#Module Global variables
+
+#Module Functions and Classes
+
+#Function to get CPU usage information of an IOS-XE device
+def xe-cpu :
+
+
+#Function to get CPU usage information of an NX device
+#def nx-cpu :
+
+#Function to get Memory usage information of an IOS-XE device
+#def xe-mem :
+
+#Function to get Memory usage information of an NX device
+#def nx-mem :
+
+#Function to get interface usage of a specific interface in an IOS-XE device
+#def xe-intf :
+
+#Function to get interface usage of a specific interface in an NX device
+#def nx-intf :
